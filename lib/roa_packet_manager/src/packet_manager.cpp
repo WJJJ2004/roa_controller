f3616b2240e5bdd5101942a30f7ec5a2341d9869
WJJJ2004 <dldnjswns7412@gmail.com>
2026-09-04 21:22:30 +0900

    ID 9 gain tune

--- lib/roa_packet_manager/src/packet_manager.cpp
+++ lib/roa_packet_manager/src/packet_manager.cpp
@@ -8,7 +8,7 @@ namespace roa_packet_manager
 
 const std::array<PacketManager::JointMeta, PacketManager::kMotorCount>
 PacketManager::kJointMetaTable{{
-  {"torso_yaw",          9,   50.0f,   2.0f},
+  {"torso_yaw",          9,   75.0f,   2.5f},
   {"left_hip_pitch",    10,  150.0f,  24.722f},
   {"right_hip_pitch",   11,  150.0f,  24.722f},
   {"left_hip_roll",     12,  200.0f,  26.387f},
